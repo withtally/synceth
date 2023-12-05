bedaacc576eb6ed0298257158d35d83c8823868a
Jemil Ezzet <jemil.ezzet@gmail.com>
2023-12-05 14:38:59 -0800

    feat(pkg-alias): add test

--- example/ethgen.go
+++ example/ethgen.go
@@ -8,14 +8,23 @@ import (
 
 	"github.com/withtally/synceth/codegen"
 	"github.com/withtally/synceth/example"
+	testexample "github.com/withtally/synceth/testexample/example"
 )
 
 func main() {
+	tea := "testexample"
 	if err := codegen.GenerateBindings("./artifacts", "./bindings", &codegen.BindingsConfig{
 		Fakes: true,
 		Handlers: codegen.HandlersConfig{
-			Generate:   true,
-			InputTypes: []codegen.InputType{{Name: "tx", Type: &example.TestInput{}}},
+			Generate: true,
+			InputTypes: []codegen.InputType{
+				{
+					Name: "tx", Type: &example.TestInput{},
+				},
+				{
+					Name: "testtx", Type: &testexample.TestInput{}, Alias: &tea,
+				},
+			},
 		},
 	}); err != nil {
 		log.Fatalf("running ethgen codegen: %v", err)
